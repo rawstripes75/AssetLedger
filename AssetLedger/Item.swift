eae5a3dbedaac14649cc2e21fb8b0c4a94e6bfb9
Tomas Turcsanyi <tomas.turcsanyi@icloud.com>
2026-04-16 11:43:42 +0200

    Initial Commit

---
+++ AssetLedger/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AssetLedger
+//
+//  Created by Tomas Turcsanyi on 16.04.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
